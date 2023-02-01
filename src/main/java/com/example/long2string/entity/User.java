383f2251c05f6d716e35141810700647f4570b66
James Hopbourn <jameshopbourn@gmail.com>
2023-02-02 00:12:04 +0800

    @TableField
    Signed-off-by: James Hopbourn <jameshopbourn@gmail.com>

--- src/main/java/com/example/long2string/entity/User.java
+++ src/main/java/com/example/long2string/entity/User.java
@@ -1,14 +1,18 @@
 package com.example.long2string.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.example.long2string.common.enums.PrivacyTypeEnum;
 import com.example.long2string.common.utils.PrivacyEncrypt;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
@@ -17,13 +21,23 @@ public class User {
     @TableId
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonSerialize(using = ToStringSerializer.class)
-    @Column(nullable = false, columnDefinition = "integer(20)")
+    @Column(nullable = false, length = 20)
     private Long adminId;
 
-    @Column(nullable = false, columnDefinition = "text(20)")
+    @Column(nullable = false, length = 10)
     private String userName;
 
     @PrivacyEncrypt(type = PrivacyTypeEnum.PHONE)
-    @Column(columnDefinition = "integer(13)")
+    @Column(length = 13)
     private String phoneNumber;
+
+    @Column(nullable = false)
+    @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    @Column(nullable = false)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
 }
---
+++ src/main/java/com/example/long2string/handler/MyMetaObjectHandler.java
@@ -0,0 +1,35 @@
+package com.example.long2string.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+/**
+ * @program: our-task
+ * @description: 对数据库每条记录的创建时间和更新时间自动进行填充
+ * @author: water76016
+ * @create: 2020-11-24 10:53
+ **/
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
